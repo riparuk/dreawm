fa603efbf06c598877e116c56be08f83f997bc7b
riparuk <faruqirifa@gmail.com>
2023-05-18 01:43:26 +0700

    Add/Mod For Welcome Page

---
+++ resources/views/components2/navbar.blade.php
@@ -0,0 +1,11 @@
+
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+        <a href="/" class="flex items-center">
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Dreawm</span>
+        </a>
+        <div class="flex items-center">
+            <a href="{{ route('register') }}" class="text-sm  text-blue-600 dark:text-blue-500 hover:underline">Register</a>
+        </div>
+    </div>
+</nav>
