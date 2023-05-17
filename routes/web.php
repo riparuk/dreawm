3d0d96bd135bcd04ae209c3b9d6c69eee22817f9
riparuk <faruqirifa@gmail.com>
2023-05-18 00:29:11 +0700

    add route to /blog

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/blog', function () {
+    return view('blog');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
