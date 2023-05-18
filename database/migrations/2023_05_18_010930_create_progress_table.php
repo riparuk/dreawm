a3929fdb41434960b66af9cd3fa1bbc3bdefc8b0
riparuk <faruqirifa@gmail.com>
2023-05-18 08:46:33 +0700

    Change atribut name of Progress table

--- database/migrations/2023_05_18_010930_create_progress_table.php
+++ database/migrations/2023_05_18_010930_create_progress_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('progress', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->text('description');
             $table->unsignedBigInteger('dream_id');
             $table->timestamps();
 
